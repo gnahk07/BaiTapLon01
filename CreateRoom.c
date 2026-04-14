42d5a5d222df2e17e9929b348c0ae0166b428ea5
gnahk07 <gnahk7753@gmail.com>
2026-04-14 16:01:51 +0700

    Thêm tính năng thêm tầng
    tạo ra 2 lựa chọn, thêm tầng và thêm phòng. Đã hoàn thiện thêm tầng

--- CreateRoom.c
+++ CreateRoom.c
@@ -1,7 +1,9 @@
 //Thư viện sử dụng
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 #include <dirent.h>
+#include <direct.h>
 #include <sys/stat.h>
 #include <string.h>
 
@@ -16,6 +18,13 @@ void countFloors(int *floorCount) {
     //Mở thư mục FloorList
     struct dirent *dir;
     DIR *dp = opendir("./FloorList");
+
+    //Kiểm tra thư mục có tồn tại không
+    if (dp == NULL) {
+        *floorCount = 0;
+        closedir(dp);
+        return;
+    }
 
     //Đếm số tầng bằng cách đếm số thư mục con trong FloorList
     while ((dir = readdir(dp)) != NULL) {
@@ -46,7 +55,7 @@ void openFloorList(int floorCount) {
     //Kiểm tra nếu không có tầng nào thì thông báo và kết thúc chương trình
     if (floorCount == 0) {
         printf("Hiện chưa có tầng nào.\n");
-        exit(0);
+        return;
     } 
     
     //Nếu có tầng thì in danh sách tầng
@@ -61,6 +70,50 @@ void openFloorList(int floorCount) {
 
     //Xóa dấu phẩy cuối cùng và thay bằng dấu chấm
     printf("\b\b.");
+}
+
+//Hàm thêm tầng
+void addFloor() {
+
+    //Hỏi người dùng có chắc chắn muốn thêm tầng mới không
+    char confirm;
+    printf("Bạn có chắc chắn muốn thêm tầng mới không? (y/n): ");
+    scanf(" %c", &confirm);
+
+    //Nếu người dùng xác nhận muốn thêm tầng mới thì thực hiện tạo thư mục mới cho tầng mới
+    if (confirm == 'y' || confirm == 'Y') {
+
+        //Tạo thư mục mới cho tầng mới
+        char path[256];
+        int floorCount = 0;
+        countFloors(&floorCount);
+        sprintf(path, "./FloorList/Tang_%d", floorCount + 1);
+        mkdir(path);
+        printf("Tầng mới '%d' đã được tạo thành công.\n", floorCount + 1);
+    } else {
+        printf("Hủy thêm tầng mới.\n");
+    }
+}
+
+//Lựa chọn thêm tầng hoặc thêm phòng
+void chooseAddOption() {
+
+    //Yêu cầu người dùng nhập lựa chọn
+    int choice;
+    printf("\nBạn muốn thêm gì? (1: Thêm tầng, 2: Thêm phòng): ");
+    scanf("%d", &choice);
+
+    //Xử lý lựa chọn của người dùng
+    if (choice == 1) {
+        //Gọi hàm thêm tầng
+        addFloor();
+    } else if (choice == 2) {
+        //Gọi hàm thêm phòng
+        //addRoom();
+    } else {
+        printf("Lựa chọn không hợp lệ. Vui lòng chọn lại.\n");
+        chooseAddOption();
+    }
 }
 
 
@@ -82,6 +135,8 @@ int main () {
     //Mở danh sách tầng
     openFloorList(floorCount);
 
+    //Lựa chọn thêm tầng hoặc thêm phòng và gọi hàm tương ứng
+    chooseAddOption();
 
     return 0;
 }
