441c3bd449831483b4de19453c4e2591046f2fcf
gnahk07 <gnahk7753@gmail.com>
2026-04-15 22:35:38 +0700

    Thêm tính năng tạo phòng
    Update: tính năng tạo phòng

--- CreateRoom.c
+++ CreateRoom.c
@@ -19,11 +19,15 @@ void countFloors(int *floorCount) {
     struct dirent *dir;
     DIR *dp = opendir("./FloorList");
 
-    //Kiểm tra thư mục có tồn tại không
+    //Kiểm tra thư mục có tồn tại không. Nếu không tồn tại thì tạo thư mục FloorList
     if (dp == NULL) {
+
+        //Tạo thư mục FloorList
+        char path[25] = "FloorList";
+        mkdir(path);
+
+        //Đặt số tầng bằng 0
         *floorCount = 0;
-        closedir(dp);
-        return;
     }
 
     //Đếm số tầng bằng cách đếm số thư mục con trong FloorList
@@ -97,8 +101,105 @@ void addFloor() {
     }
 }
 
+//Hàm kiểm tra lựa chọn tầng để thêm phòng có phù hợp không
+void checkSelectFloor(int selectFloor, int floorCount, int *check) {
+
+    //Kiểm tra lựa chọn
+    for (int i = 1; i <= floorCount; i++) {
+        if (selectFloor == i) {
+            *check = 1;
+        }
+    }
+}
+
+//In các phòng đã có sẵn
+void displayRoom(int selectFloor){
+
+    //Mở thư mục chứa phòng
+    char path[256];
+    sprintf(path, "./FloorList/Tang_%d", selectFloor);
+    struct dirent *dir;
+    DIR *dp = opendir(path);
+
+    //Kiểm tra có phòng nào không
+    if (dp == NULL) {
+        printf("Không có phòng nào ở tầng %d.\n", selectFloor);
+        return;
+    }
+
+    //In các phòng hiện có
+    printf("Hiện có sẵn các phòng: ");
+    while ((dir = readdir(dp)) != NULL) {
+
+        //Lấy tên File và tính độ dài của file
+        char *name = dir->d_name;
+        int len = strlen(name);
+
+        //Kiểm tra tên phòng với đuôi file là .txt và in ra tên phòng
+        if (len > 4 && strcmp(name + len - 4, ".txt") == 0) {
+            printf("%.*s, ", len - 4, name);
+        }
+    }
+
+    //Xóa dấu phẩy cuối cùng và thay bằng dấu chấm
+    printf("\b\b.");
+
+    //Đóng thư mục sau khi in dãy phòng xong
+    closedir(dp);
+}
+
+//Hàm thêm phòng
+void addRoom(int floorCount) {
+
+    //In các tầng hiện có
+    printf ("");
+
+    //Lựa chọn tầng muốn thêm phòng
+    int selectFloor;
+    printf("Chọn tầng muốn thêm phòng: ");
+    scanf("%d", &selectFloor);
+
+    //Kiểm tra lựa chọn có phù hợp không
+    int check = 0;
+    checkSelectFloor(selectFloor, floorCount, &check);
+
+    //Kiểm tra lựa chọn có phù hợp không nếu đã phù hợp thì tạo phòng
+    if (check == 1) {
+        //Gọi hàm in ra các dãy phòng đã có sẵn
+        displayRoom(selectFloor);
+
+        //Nhập tên phòng
+        char roomName[128];
+        printf("\nNhập tên phòng mới: ");
+        scanf(" %127s", roomName);
+
+        //Tạo đường dẫn đầy đủ đến nơi chứa tên phòng
+        char roomPath[256];
+        sprintf(roomPath, "./FloorList/Tang_%d/%s.txt", selectFloor, roomName);
+
+        //Tạo file chứa dữ liệu
+        FILE *fp = fopen(roomPath, "r");
+        if (fp != NULL) {
+            fclose(fp);
+            printf("Phòng %s đã tồn tại.\n", roomName);
+        } else {
+            fp = fopen(roomPath, "w");
+            if (fp != NULL) {
+                fclose(fp);
+                printf("Phòng %s đã được tạo thành công.\n", roomName);
+            } else {
+                printf("Không thể tạo phòng. Vui lòng kiểm tra quyền truy cập.\n");
+            }
+        }
+    } else {
+        //Cho người dùng chọn lại phòng
+        printf("Tầng bạn chọn không tồn tại, chọn lại tầng.\n");
+        addRoom(floorCount);
+    }
+}
+
 //Lựa chọn thêm tầng hoặc thêm phòng
-void chooseAddOption() {
+void chooseAddOption(int floorCount) {
 
     //Yêu cầu người dùng nhập lựa chọn
     int choice;
@@ -110,11 +211,17 @@ void chooseAddOption() {
         //Gọi hàm thêm tầng
         addFloor();
     } else if (choice == 2) {
-        //Gọi hàm thêm phòng
-        //addRoom();
+        //Kiểm tra đã có tầng nào chưa
+        if (floorCount == 0) {
+            printf("Hiện không có tầng nào. Hãy tạo thêm tầng để tạo phòng.");
+            chooseAddOption(floorCount);
+        } else {
+            //Gọi hàm thêm phòng
+            addRoom(floorCount);
+        }
     } else {
         printf("Lựa chọn không hợp lệ. Vui lòng chọn lại.\n");
-        chooseAddOption();
+        chooseAddOption(floorCount);
     }
 }
 
@@ -136,8 +243,7 @@ int main () {
     openFloorList(floorCount);
 
     //Lựa chọn thêm tầng hoặc thêm phòng và gọi hàm tương ứng
-    chooseAddOption();
-
+    chooseAddOption(floorCount);
 
     return 0;
 }
