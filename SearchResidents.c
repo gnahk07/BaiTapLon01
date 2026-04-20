67d7d9ecc75a227747f9ed3cdabc0d6f012eaef2
DucCuongPham <duccuongpham0408@gmail.com>
2026-04-20 11:01:42 +0700

    Update SearchResidents.c

--- SearchResidents.c
+++ SearchResidents.c
@@ -1,12 +1,33 @@
 //Thư viện sử dụng
 #include <stdio.h>
 #include <windows.h>
-
+typedef struct Resident{
+	char fullName[50];
+	int citiIdentNum[12];
+	struct resident *next;
+}Resident;
+//Tìm kiếm bằng tên
+void searchByName(Resident *head , char name[]){ 
+	Resident *temp=head;
+	int found=0;
+	
+	
+}
 //Hiển thị Header
 void displayHeader() {
     printf("\n=====================================  PHẦN MỀM TÌM KIẾM CƯ DÂN  =====================================\n\n");
 }
-
+void searchMenu(Resident *head){ //Menu tìm kiếm
+	int choice;
+	do{
+		printf("1.Tìm kiếm bằng tên.\n");
+		printf("2.Tìm kiếm bằng số CCCD.\n");
+		printf("0.Quay trở lại.\n");
+		printf("Hãy nhập lựa chọn (1,2 hoặc 0): ");
+		scanf("%d",&choice);
+		getchar();
+	}
+}
 
 
 
