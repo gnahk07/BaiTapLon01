8659c54d9dbade225fa14bd6a50d71487331e825
DucCuongPham <duccuongpham0408@gmail.com>
2026-04-21 20:37:48 +0700

    Update SearchResidents.c

--- SearchResidents.c
+++ SearchResidents.c
@@ -1,37 +1,39 @@
 //Thư viện sử dụng
 #include <stdio.h>
 #include <windows.h>
-//Tìm kiếm bằng tên
-void searchByName(Resident *head , char name[]){ 
-	Resident *temp=head;
-	int found=0;
-}
-//Tìm kiếm bằng CCCD
-void searchByCCCD(Resident *head , int CCCD[]){
-	
-}
-
 //Hiển thị Header
 void displayHeader() {
     printf("\n=====================================  PHẦN MỀM TÌM KIẾM CƯ DÂN  =====================================\n\n");
 }
+//Tìm kiếm bằng tên hoặc CCCD
 void searchMenu(Resident *head){ //Menu tìm kiếm
-	int choice;
-	do{
-		printf("1.Tìm kiếm bằng tên.\n");
-		printf("2.Tìm kiếm bằng số CCCD.\n");
-		printf("0.Quay trở lại.\n");
-		printf("Hãy nhập lựa chọn (1,2 hoặc 0): ");
-		scanf("%d",&choice);
-		getchar();
+	if (head==NULL){
+		printf("Hiện tại chưa có dữ liệu phòng nào trong hệ thống!");
+		return 0;
 	}
+	int choice;
+	char searchKey[50];
+	int found=0;
+
+	printf("1.Tìm kiếm bằng tên.\n");
+	printf("2.Tìm kiếm bằng số CCCD.\n");
+	printf("Hãy nhập lựa chọn (1 hoặc 2): ");
+	scanf("%d",&choice);
+	getchar();
+
+	printf("Nhập thông tin cần tìm: ");
+	fgets(searchKey, sizeof(searchKey), stdin);
+    searchKey[strcspn(searchKey, "\n")] = 0; // Xóa ký tự xuống dòng
+	
+	
+		
 }
 
 
 
 //Hàm main
 int main () {
-
+	
     //chỉnh sửa lỗi front chữ 
 	SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
@@ -39,6 +41,7 @@ int main () {
     //Hiển thị Header
     displayHeader ();
 
-
+	//Hiển thị menu
+	searchMenu(*head);
     return 0;
 }
